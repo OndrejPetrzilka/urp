13e596675d128f714635c781d0f13f90b74ad09e
Ondrej Petrzilka <ondrej.petrzilka@gmail.com>
2026-02-10 14:49:54 +0100

    * upgraded to 17.4.0, Unity 4.0b7_228b727801f0

--- Runtime/Data/PostProcessData.cs
+++ Runtime/Data/PostProcessData.cs
@@ -20,9 +20,9 @@ namespace UnityEngine.Rendering.Universal
     {
 #if UNITY_EDITOR
         [SuppressMessage("Microsoft.Performance", "CA1812")]
-        internal class CreatePostProcessDataAsset : EndNameEditAction
+        internal class CreatePostProcessDataAsset : AssetCreationEndAction
         {
-            public override void Action(int instanceId, string pathName, string resourceFile)
+            public override void Action(EntityId entityId, string pathName, string resourceFile)
             {
                 var instance = CreateInstance<PostProcessData>();
                 AssetDatabase.CreateAsset(instance, pathName);
@@ -34,7 +34,7 @@ namespace UnityEngine.Rendering.Universal
         static void CreatePostProcessData()
         {
             var icon = EditorGUIUtility.IconContent("ScriptableObject Icon").image as Texture2D;
-            ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, CreateInstance<CreatePostProcessDataAsset>(), "CustomPostProcessData.asset", icon, null);
+            ProjectWindowUtil.StartNameEditingIfProjectWindowExists(EntityId.None, CreateInstance<CreatePostProcessDataAsset>(), "CustomPostProcessData.asset", icon, null);
         }
 
         internal static PostProcessData GetDefaultPostProcessData()
